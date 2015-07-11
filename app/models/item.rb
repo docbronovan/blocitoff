c03c489f6cbb0bf5d3d70ce7e9660bfc548d3ed9
docbronovan <brockdonovan@gmail.com>
2015-07-10 21:04:33 -0400

    add partials

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  validates :description, length: {minimum: 1}, presence: true
+  validates :user_id, presence: true
+
+  default_scope { order('updated_at') }
 end
