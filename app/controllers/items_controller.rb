db1c319ea632f87139412f4364e50089fd7fd130
docbronovan <brockdonovan@gmail.com>
2015-07-13 15:45:05 -0400

    trying to render with js

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,18 @@
 class ItemsController < ApplicationController
   def create
 
-   @item = Item.new(params.require(:item).permit(:name))
+    @item = Item.new(params.require(:item).permit(:name))
 
-   if @item.save
+    if @item.save
       redirect_to user_path(:id), notice: "Item was saved successfully."
-   else
+    else
       flash[:error] = "Error creating item. Please try again."
       render :new
-   end
+    end
 
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
