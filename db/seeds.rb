ad4bbf731d0c97ca8649b3cfaed3bdcd45fc9eeb
docbronovan <brockdonovan@gmail.com>
2015-07-15 09:25:16 -0400

    seeds complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+ 
+# Create User
+1.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+ email: 'brockdonovan@gmail.com',
+ password: 'fluffy11'
+)
+
+
+# Create items
+10.times do
+  Item.create!(
+    user:   users.sample,
+    description:  Faker::Lorem.sentence
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
