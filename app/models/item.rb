974837ba62dc3b84d908acd2a3a7eb9395168a50
docbronovan <brockdonovan@gmail.com>
2015-07-20 09:00:26 -0400

    adding extras

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ActiveRecord::Base
   default_scope { order('updated_at') }
 
   def days_left
-      7 - (DateTime.now.to_date - created_at.to_date).to_i
+      7 - (DateTime.today - created_at.to_date).to_i
   end
 
 end
